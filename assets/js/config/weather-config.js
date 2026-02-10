c26f3ae7bc6ab0e37a86708513f7db1fcb526f3d
Arnaud BREGERE <arnaud.bregere@gmail.com>
2026-02-10 11:07:38 +0100

    Clean css (#7)
    * CSS : indent + px instead of rem + clean + about page links seo/rgaa
    
    * CSS Mobile adjustments

--- assets/js/config/weather-config.js
+++ assets/js/config/weather-config.js
@@ -2,7 +2,6 @@
 
 
 // Clé API OpenWeatherMap
-// En Prod: Utiliser un backend proxy pour masquer cette clé 
 export const API_KEY = "2e126ccf8299a2eef2da7d5bf2bdf946";
 
 // Chemin vers les assets SVG
